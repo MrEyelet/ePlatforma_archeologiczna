c638f8b1830099663ed8250597b43bbb452a5be7
unknown <marcin.adam.mroczko@gmail.com>
2018-10-11 14:09:55 +0200

    hamburger animation added

--- script.js
+++ script.js
@@ -2,5 +2,9 @@ $( document ).ready(function() {
 	
     $('.mobile-menu').on('click', function(){
     	$('.panel').toggleClass('js-panel--show');
+    	$(this).toggleClass('rotate');
+        $('.hamburger__top').toggleClass('hamburger__top--toggle');
+        $('.hamburger__middle').toggleClass('hamburger__middle--toggle');
+        $('.hamburger__bottom').toggleClass('hamburger__bottom--toggle');
     });
 });
